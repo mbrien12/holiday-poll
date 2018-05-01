d12564b9f7f0bdbce3c1d72c5966eb3574264189
Michelle Brien <michelle.brien12@gmail.com>
2018-05-01 20:27:09 +0100

    added fake data file

---
+++ src/sample-holidays.js
@@ -0,0 +1,17 @@
+const holidays = {
+  holiday1: {
+    location: "Greece",
+    flightPrice: "240", 
+    reasons: ["Beaches", "Ruins", "Kebabs", "Islands"],
+  },
+  holiday2: {
+    location: "Senegal",
+    flightPrice: "500", 
+    reasons: ["African vibes"],
+  },
+  holiday3: {
+    location: "Mexico",
+    flightPrice: "807", 
+    reasons: ["Tacos", "Tequilla"],
+  }
+}
